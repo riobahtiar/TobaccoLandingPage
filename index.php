cd1c1e5148b1f88279d94585a07c8b7e890d89f2
Rio Bahtiar <akhibahtiar@gmail.com>
2015-05-15 13:29:50 +0700

    showcase updated + icon
    go ahead to costumizing showcase

--- index.php
+++ index.php
@@ -39,7 +39,8 @@
                         <li><a class="menu-link" href="#solutions">Solutions</a></li>
                         <li><a class="menu-link" href="#showcase">Showcase</a></li>
                         <li><a class="menu-link" href="#clients">Clients</a></li>
-                        <li><a class="menu-link" href="#contact-us"><i class="fa fa-search"> </i> Contact Us</a></li>
+                        <li><a class="menu-link" href="#contact-us">Contact Us</a></li>
+                        <li><a href="#search"><i class="fa fa-search"></i></a></li>
                     </ul>
                     </div><!-- /.navbar-collapse -->
                     </div><!-- /.container-fluid -->
@@ -59,6 +60,7 @@
                     <div class="container">
                         <div class="row">
                             <article class="col-lg-12 text-center">
+                            <h1><i class="fa fa-user-secret fa-4x"></i></h1>
                             <h2>About Us</h2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
                             <hr>
@@ -73,7 +75,7 @@
                                     </div>
                                     <div class="panel-footer">
                                         <blockquote>
-                                            <p>Bianka</p>
+                                            <p>Bian</p>
                                         <footer>Founder</footer>
                                     </blockquote>
                                 </div>
@@ -167,11 +169,9 @@
 </aside>
 </div>
 <!-- Row Two -->
-
 </div>
 </div>
 </section>
-
 <section id="services">
 <div class="container">
 <div class="row">
@@ -218,7 +218,8 @@
 <div class="container">
 <div class="row">
 <div class="col-lg-12 text-center">
-<h2 class="section-heading">Our Services</h2>
+<h1><i class="fa fa-leaf fa-4x"></i></h1>
+<h2 class="section-heading">Solutions</h2>
 <hr class="primary">
 </div>
 </div>
@@ -260,42 +261,108 @@
 <div class="container">
 <div class="row">
 <div class="col-lg-12 text-center">
-<h2 class="section-heading">Our Servicess</h2>
+<h1><i class="fa fa-space-shuttle fa-4x"></i></h1>
+<h2 class="section-heading">Showcase</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
 <hr class="primary">
 </div>
 </div>
 </div>
 <div class="container">
-<div class="row">
-<div class="col-lg-3 col-md-6 text-center">
-<div class="service-box">
-    <i class="fa fa-5x text-danger fa-diamond"></i>
-    <h3>Localtobacco.com</h3>
-    <p class="text-muted">Okeh bro just test text here</p>
-</div>
-</div>
-<div class="col-lg-3 col-md-6 text-center">
-<div class="service-box">
-    <i class="fa fa-5x text-danger fa-paper-plane"></i>
-    <h3>Localtobacco.com</h3>
-    <p class="text-muted">Okeh bro just test text here</p>
-</div>
-</div>
-<div class="col-lg-3 col-md-6 text-center">
-<div class="service-box">
-    <i class="fa fa-5x text-danger fa-newspaper-o"></i>
-    <h3>Localtobacco.com</h3>
-    <p class="text-muted">Okeh bro just test text here</p>
-</div>
-</div>
-<div class="col-lg-3 col-md-6 text-center">
-<div class="service-box">
-    <i class="fa fa-5x text-danger fa-heart"></i>
-    <h3>Localtobacco.com</h3>
-    <p class="text-muted">Okeh bro just test text here</p>
-</div>
-</div>
-</div>
+<!-- Showcase Begin Here -->
+    <div class="row">
+        <div class="col-md-4">
+            <!-- begin panel group -->
+            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                
+                <!-- panel 1 -->
+                <div class="panel panel-default no-border">
+                    <!--wrap panel heading in span to trigger image change as well as collapse -->
+                    <span class="side-tab" data-target="#tab1" data-toggle="tab" role="tab" aria-expanded="false">
+                        <div class="panel-heading" role="tab" id="headingOne"data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <h4 class="panel-title">Archimedes Site</h4>
+                        </div>
+                    </span>
+                    
+                    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                        <div class="panel-body">
+                        <!-- Tab content goes here -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>                        </div>
+                    </div>
+                </div> 
+                <!-- / panel 1 -->
+                
+                <!-- panel 2 -->
+                <div class="panel panel-default no-border">
+                    <!--wrap panel heading in span to trigger image change as well as collapse -->
+                    <span class="side-tab" data-target="#tab2" data-toggle="tab" role="tab" aria-expanded="false">
+                        <div class="panel-heading" role="tab" id="headingTwo" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <h4 class="panel-title collapsed">Graha Sarina Vidi Site</h4>
+                        </div>
+                    </span>
+
+                    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                        <div class="panel-body">
+                        <!-- Tab content goes here -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>                        </div>
+                    </div>
+                </div>
+                <!-- / panel 2 -->
+                
+                <!--  panel 3 -->
+                <div class="panel panel-default no-border">
+                    <!--wrap panel heading in span to trigger image change as well as collapse -->
+                    <span class="side-tab" data-target="#tab3" data-toggle="tab" role="tab" aria-expanded="false">
+                        <div class="panel-heading" role="tab" id="headingThree"  class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <h4 class="panel-title">Shoe Care Site</h4>
+                        </div>
+                    </span>
+
+                        <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                          <div class="panel-body">
+                          <!-- tab content goes here -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>                          </div>
+                        </div>
+                      </div>
+            </div> <!-- / panel-group -->
+            <button class="btn btn-see-all pull-right">See All <i class="fa fa-circle"></i></button>
+             
+        </div> <!-- /col-md-4 -->
+        
+        <div class="col-md-8">
+            <!-- begin macbook pro mockup -->
+            <div class="md-macbook-pro md-glare">
+                <div class="md-lid">
+                    <div class="md-camera"></div>
+                    <div class="md-screen">
+                    <!-- content goes here -->                
+                        <div class="tab-featured-image">
+                            <div class="tab-content">
+                                <div class="tab-pane  in active" id="tab1">
+                                        <img src="https://ununsplash.imgix.net/photo-1417024321782-1375735f8987?dpr=2&fit=crop&fm=jpg&h=650&q=75&w=950" alt="tab1" class="img img-responsive">
+                                </div>
+                                <div class="tab-pane " id="tab2">
+                                    
+                                        <img src="https://unsplash.imgix.net/uploads/1411724908903377d4696/2e9b0cb2?dpr=2&fit=crop&fm=jpg&h=650&q=75&w=950">
+                                    
+                                </div>
+                                <div class="tab-pane fade" id="tab3">
+                                    
+                                        <img src="https://ununsplash.imgix.net/photo-1422479516648-9b1f0b6e8da8?dpr=2&fit=crop&fm=jpg&h=650&q=75&w=950" alt="tab1" class="img img-responsive">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="md-base"></div>
+            </div> <!-- end macbook pro mockup -->
+
+
+
+        </div> <!-- / .col-md-8 -->
+    </div> <!--/ .row -->
+<!-- /Showcase -->
 </div>
 </section>
 <section id="clients">
@@ -382,12 +449,11 @@
 </div>
 </div>
 <div id="wrap">
-  <form action="" autocomplete="on">
-  <input id="search" name="search" type="text" placeholder="What u're looking for? "><input id="search_submit" value="Rechercher" type="submit">
-  </form>
+<form action="" autocomplete="on">
+<input id="search" name="search" type="text" placeholder="What u're looking for? "><input id="search_submit" value="Rechercher" type="submit">
+</form>
 </div>
 </section>
-
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="assets/js/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
